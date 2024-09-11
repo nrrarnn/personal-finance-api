1b04b082b645e9598bae3441699d254152516e64
Nur Arini <nurarini0302@gmail.com>
2024-09-11 13:59:33 +0700

    [UPDATE] update register

--- controllers/register.js
+++ controllers/register.js
@@ -5,25 +5,20 @@ exports.register = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    // Cek apakah user sudah ada
-    // let user = await User.findOne({ email });
-    // if (user) {
-    //   return res.status(400).json({ message: 'User already exists' });
-    // }
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
 
-    // Buat user baru
     user = new User({ username, email, password });
 
-    // Simpan user ke database
     await user.save();
 
-    // Buat JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    // Kirim respons
     res.status(201).json({ token });
   } catch (error) {
-    console.error(error); // Tambahkan informasi error untuk debugging
+    console.error(error); 
     res.status(500).json({ message: 'Server error' });
   }
 };
